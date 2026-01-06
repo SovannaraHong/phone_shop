14c13c59cc49218600622af81189685962b29614
Hong Sovannara <rapewpew372@gmail.com>
2026-01-06 09:38:03 +0700

    add EqualsAndHashCode

--- src/main/java/com/phone_shop/phoneshop/exception/ApiException.java
+++ src/main/java/com/phone_shop/phoneshop/exception/ApiException.java
@@ -1,10 +1,12 @@
 package com.phone_shop.phoneshop.exception;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @RequiredArgsConstructor
 public class ApiException extends RuntimeException {
