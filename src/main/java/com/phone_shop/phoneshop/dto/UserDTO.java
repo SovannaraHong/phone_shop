5b8e9c6df42aadfa2fedaf7a5a8216ec766f693b
SovannaraHong <rapewpew372@gmail.com>
2026-02-18 21:31:00 +0700

    add

--- src/main/java/com/phone_shop/phoneshop/dto/UserDTO.java
+++ src/main/java/com/phone_shop/phoneshop/dto/UserDTO.java
@@ -1,4 +1,10 @@
 package com.phone_shop.phoneshop.dto;
 
 public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    
 }
